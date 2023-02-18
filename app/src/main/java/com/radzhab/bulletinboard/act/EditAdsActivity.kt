5e18be9419fe3cf929d2396ecc2b2f1b7edb2ed6
RadSham <radsham9@gmail.com>
2023-02-19 00:28:12 +0300

    Create an EditActivity

--- app/src/main/java/com/radzhab/bulletinboard/act/EditAdsActivity.kt
+++ app/src/main/java/com/radzhab/bulletinboard/act/EditAdsActivity.kt
@@ -2,7 +2,9 @@ package com.radzhab.bulletinboard.act
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.radzhab.bulletinboard.databinding.ActivityEditAdsBinding
+import com.radzhab.bulletinboard.utils.CityHelper
 
 class EditAdsActivity : AppCompatActivity() {
     private lateinit var rootElement: ActivityEditAdsBinding
@@ -12,5 +14,13 @@ class EditAdsActivity : AppCompatActivity() {
         rootElement = ActivityEditAdsBinding.inflate(layoutInflater)
         val view = rootElement.root
         setContentView(view)
+
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            CityHelper.getAllCountries(this)
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        rootElement.spCountry.adapter = adapter
     }
 }
